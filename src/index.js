304d0e28b41b0e0474c8ec49242a1c5c3aa801df
marinamelnik1105 <masha.nasha119@gmail.com>
2023-01-18 17:21:10 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -27,14 +27,31 @@ async function onSearch(e) {
   console.dir(e.target.elements.searchQuery.value);
   const searchQuery = e.target.elements.searchQuery.value;
   const observer = new IntersectionObserver(onInfinityLoad, optionsForObserv);
+
+  if (searchQuery === '') {
+    await clearImages();
+    return;
+  }
   const result = await fetchImage(searchQuery);
   console.log(result);
-  currentHits = result.hits;
-  clearImages();
-  createMarkup(result.hits);
-  observer.observe(refs.guard);
+  try {
+    if (result.totalHits > 0) {
+      Notiflix.Notify.success(`Hooray! We found ${result.totalHits} images.`);
+      await clearImages();
+      await createMarkup(result.hits);
+      observer.observe(refs.guard);
+    }
+    if (!result.totalHits) {
+      await clearImages();
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    }
+  } catch (err) {
+    console.log(err);
+  }
 
-  function createMarkup(images) {
+  async function createMarkup(images) {
     const markup = images
       .map(
         ({
@@ -68,7 +85,7 @@ async function onSearch(e) {
     refs.gallery.insertAdjacentHTML('beforeend', markup);
   }
 
-  function clearImages() {
+  async function clearImages() {
     refs.gallery.innerHTML = '';
   }
   async function onInfinityLoad(entries) {
@@ -77,15 +94,24 @@ async function onSearch(e) {
         page += 1;
         hits += 30;
         const result = await fetchImage(page);
-        createMarkup(result.hits);
-        if (hits >= result.hits) {
-          observer.unobserve(refs.guard);
+        try {
+          if (result.totalHits > 0) {
+            await createMarkup(result.hits);
+            if (hits >= result.hits) {
+              observer.unobserve(refs.guard);
+            }
+          }
+          if (!result.totalHits) {
+            await clearImages();
+          }
+        } catch (err) {
+          console.log(err);
         }
       }
     });
   }
 }
 async function fetchImage(q) {
-  const url = `${BASE_URL}?key=${API_KEY}&q=${q}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=30`;
+  const url = `${BASE_URL}?key=${API_KEY}&q=${q}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`;
   return await axios.get(url).then(response => response.data);
 }
